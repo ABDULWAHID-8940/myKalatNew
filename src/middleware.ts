4988d3cc11c26af4dc3620217a6e2eadee4a27eb
fikiryilkal tages <fikeryilkaltages@gmail.com>
2025-11-14 12:04:22 +0300

    fix: redirect to callback

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,21 @@ import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
-  // const sessionCookie = request.cookies.get("better-auth.session_token");
-
   const pathname = request.nextUrl.pathname;
-  console.log({ pathname, sessionCookie });
 
-  // If no cookie → redirect away from protected routes
-  // if (!sessionCookie) {
-  //   if (
-  //     pathname.startsWith("/influencer") ||
-  //     pathname.startsWith("/business")
-  //   ) {
-  //     return NextResponse.redirect(new URL("/auth", request.url));
-  //   }
-  //   return NextResponse.next();
-  // }
+  if (!sessionCookie) {
+    if (
+      pathname.startsWith("/influencer") ||
+      pathname.startsWith("/business")
+    ) {
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
+    return NextResponse.next();
+  }
 
-  // // If authenticated → prevent access to /auth or /
-  // if (sessionCookie && (pathname === "/" || pathname.startsWith("/auth"))) {
-  //   return NextResponse.redirect(new URL("/influencer", request.url));
-  // }
+  if (sessionCookie && (pathname === "/" || pathname.startsWith("/auth"))) {
+    return NextResponse.redirect(new URL("/api/auth/callback", request.url));
+  }
 
   return NextResponse.next();
 }
