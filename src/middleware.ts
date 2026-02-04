a2ae49e8fa37ab844d64a5b78112c5b930da4753
fikiryilkal tages <fikeryilkaltages@gmail.com>
2026-02-04 20:26:43 +0300

    fix: added middlewere back

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+  const pathname = request.nextUrl.pathname;
+
+  if (!sessionCookie) {
+    if (
+      pathname.startsWith("/influencer") ||
+      pathname.startsWith("/business")
+    ) {
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (sessionCookie && (pathname === "/" || pathname.startsWith("/auth"))) {
+    return NextResponse.redirect(new URL("/api/auth/callback", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/auth", "/influencer/:path*", "/business/:path*"],
+};
